217ac23a5f067790a34f61b385d7ecdce1145dfc
Haotian He <haotianh@uw.edu>
2014-03-18 02:25:36 -0700

    c

---
+++ hobbs.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+LING571 HW7
+
+Created on Thu Mar 18 17:25:09 2014
+
+@author: haotianhe
+"""
+
+import nltk
+import sys
+
+if __name__ == "__main__":
+
+    
---
+++ hw7.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# LING571 HW7
+# Haotian He
+# Created on Thu Mar 18 22:08:06 2014
+
+python2.7 hobbs.py $@

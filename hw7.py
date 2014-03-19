ec946bcfd4244c7e9769d350b7d5de052af32dfc
Haotian He <haotianh@uw.edu>
2014-03-18 19:35:03 -0700

    c

--- hw7.py
+++ hw7.py
@@ -8,34 +8,62 @@ Created on Thu Mar 18 17:25:09 2014
 """
 
 import nltk
+import pprint
 import sys
 
-if __name__ == "__main__":
+def printfunc(sent_pair):
 
+    tree_1 = parser.nbest_parse(sent_pair[0], 1)
+    tree_2 = parser.nbest_parse(sent_pair[1], 1)
+
+    s_1 = nltk.tree.ParentedTree.convert(tree_1[0])
+    s_2 = nltk.tree.ParentedTree.convert(tree_2[0])
+
+    pron = get_pron(str(s_2))
+    proposed_nodes = []
+    case = None
+        for x in s_1.subtrees():
+            temp = x.node.__repr__()[:2]            
+            if temp == 'S[':
+                proposed_nodes.append((x,case))
+            elif temp == 'NP':
+                parent = x.parent().node.__repr__()[:2]                
+                if parent == 'VP':
+                    case= 'ACC'               
+                proposed_nodes.append((x,case))
+
+    print "i) " + pron + s_1.pprint(margin=500) + s_2.pprint(margin=500)
+    print "A) " + s_1.pprint(margin=500) 
+    print "A) " + s_2.pprint(margin=500)
+    proposed_print(proposed_nodes)
+    print ""
+
+def get_pron(s):
+    tokens = s.split(' ')
+    for i in range(len(tokens)):
+        if "(PRP[]" in tokens[i]:
+            pron = tokens[i + 1]
+            break
+    return pron[:-3]
+
+if __name__ == "__main__":
     if (len(sys.argv) >= 2):
         grammar = nltk.data.load("file:" + sys.argv[1])
         sentences = open(sys.argv[2], 'r')
     else:
-        grammar = nltk.data.load("file:grammar.cfg")
+        grammar = nltk.data.load("file:grammar.fcfg")
         sentences = open("coref_sentences.txt", 'r')
 
     parser = nltk.parse.FeatureEarleyChartParser(grammar)
 
-    SentencePair = []
-
+    sent_num = 0
+    sent_pair = []
     for sent in sentences:
-        if sent == "":
-            continue
-
+        sent_num += 1
         sent = nltk.tokenize.wordpunct_tokenize(sent)
-        SentencePair.append(sent)
-
-        if len(SentencePair) == 2:
-            sent_1 = parser.nbest_parse(SentencePair[0], 1)
-            sent_2 = parser.nbest_parse(SentencePair[1], 1)
-            if len(sent_1) == 1 and len(sent_2) == 1:
-                treeToString = str(sent_1)
-                print sent_1.pprint(margin=500) + '\n'
-                print '\n'
+        sent_pair.append(sent)
+        if sent_num % 3 == 0:
+            printfunc(sent_pair)
+            sent_pair = []
 
     sentences.close()
